9eeea5c722644c1b708eb8a6ec2183a964c24222
NicRSkinner <nicholas.skinner95@gmail.com>
2020-12-06 14:51:11 -0600

    Corrected component registration.

--- include/input_calibration_node.h
+++ include/input_calibration_node.h
@@ -25,11 +25,4 @@ namespace bfr
     };
 } // namespace bfr
 
-#include "rclcpp_components/register_node_macro.hpp"
-
-// Register the component with class_loader.
-// This acts as a sort of entry point, allowing the component to be discoverable when its library
-// is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(bfr::InputCalibrationNode)
-
 #endif // !_INPUT_CALIBRATION_NODE_
