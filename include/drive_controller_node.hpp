c071d34591a1669cf78b5caf60bbc67afdfcb56a
NicRSkinner <nicholas.skinner95@gmail.com>
2021-01-25 17:13:27 -0600

    Added basic drive controller with parameter callbacks.

---
+++ include/drive_controller_node.hpp
@@ -0,0 +1,31 @@
+#ifndef _DRIVE_CONTROLLER_NODE_H_
+#define _DRIVE_CONTROLLER_NODE_H_
+
+#include "rclcpp/rclcpp.hpp"
+#include "rcl_interfaces/msg/set_parameters_result.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "bfr_msgs/msg/gamepad.hpp"
+
+namespace bfr
+{
+    class DriveControllerNode : public rclcpp::Node
+    {
+    public:
+        explicit DriveControllerNode(const rclcpp::NodeOptions &options);
+
+    private:
+        void loop();
+        
+        void gamepad_callback(const bfr_msgs::msg::Gamepad::SharedPtr msg) const;
+        rcl_interfaces::msg::SetParametersResult parametersCallback(
+            const std::vector<rclcpp::Parameter> &parameters);
+
+        bool gamepadEquipped = false;
+
+        bfr_msgs::msg::Gamepad lastReceivedMessage;
+        rclcpp::TimerBase::SharedPtr loopTimer;
+        rclcpp::Subscription<bfr_msgs::msg::Gamepad>::SharedPtr gamepadSubscription;
+    };
+} // namespace bfr
+
+#endif // !_DRIVE_CONTROLLER_NODE_H_
---
+++ src/drive_controller_node.cpp
@@ -0,0 +1,64 @@
+#include "drive_controller_node.hpp"
+
+#include <chrono>
+#include <iostream>
+
+using namespace std::literals::chrono_literals;
+using std::placeholders::_1;
+
+namespace bfr
+{
+    DriveControllerNode::DriveControllerNode(const rclcpp::NodeOptions &options) : Node("DriveController", options)
+    {
+        this->declare_parameter("gamepadEquipped", false);
+        this->get_parameter("gamepadEquipped", this->gamepadEquipped);
+
+        this->set_on_parameters_set_callback(
+            std::bind(&DriveControllerNode::parametersCallback, this, std::placeholders::_1));
+
+        if (this->gamepadEquipped)
+        {
+            this->gamepadSubscription = this->create_subscription<bfr_msgs::msg::Gamepad>(
+                "hal/inputs/gamepad", 10, std::bind(&DriveControllerNode::gamepad_callback, this, _1)
+            );
+        }
+
+        this->loopTimer = this->create_wall_timer(50ms, std::bind(&DriveControllerNode::loop, this));
+    }
+
+    rcl_interfaces::msg::SetParametersResult DriveControllerNode::parametersCallback(
+        const std::vector<rclcpp::Parameter> &parameters)
+    {
+        rcl_interfaces::msg::SetParametersResult result;
+        result.successful = true;
+        result.reason = "NA";
+
+        for (const auto &parameter : parameters)
+        {
+            if (parameter.get_name() == "gamepadEquipped" &&
+                parameter.get_type() == rclcpp::ParameterType::PARAMETER_BOOL)
+            {
+                this->gamepadEquipped = parameter.as_bool();
+            }
+        }
+    }
+
+    void DriveControllerNode::gamepad_callback(const bfr_msgs::msg::Gamepad::SharedPtr msg) const
+    {
+
+    }
+
+    void DriveControllerNode::loop()
+    {
+
+    }
+}
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+
+// Ignore linting errors stating "int assumed" this seems to be a VSCode issue.
+RCLCPP_COMPONENTS_REGISTER_NODE(bfr::DriveControllerNode)
