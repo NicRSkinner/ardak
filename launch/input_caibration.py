d91950b182b0eadc67b8c7a7a4d5f368f943f304
NicRSkinner <nicholas.skinner95@gmail.com>
2021-02-06 09:50:05 -0600

    Add more launch files.

---
+++ launch/input_caibration.py
@@ -0,0 +1,31 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
+
+    container = ComposableNodeContainer(
+        name='launch_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='ardak',
+                plugin='bfr::InputCalibrationNode',
+                name='InputCalibrationNode'
+            )
+        ],
+        output="screen"
+    )
+
+    ld.add_action(container)
+
+    return ld
