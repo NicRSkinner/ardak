5fb0d00e776edb66bf300ad1d4064edeea600ea7
NicRSkinner <nicholas.skinner95@gmail.com>
2021-12-30 14:10:39 -0600

    Corrected errors in launch file.

--- launch/ardak_localization.launch.py
+++ launch/ardak_localization.launch.py
@@ -4,8 +4,8 @@
 # Launch map algo
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
 from launch_ros.actions import Node
 
 
@@ -24,19 +24,18 @@ def generate_launch_description():
           ('rgb/camera_info', 'rs_d435/image_raw/camera_info'),
           ('depth/image', '/rs_d435/aligned_depth/image_raw')]
 
-    ld = LaunchDescription()
+    return LaunchDescription([
 
-    t265_node = Node(
+    Node(
         package='realsense_ros2',
-        node_executable='rs_t265_node',
-        node_name='rs_t265',
+        executable='rs_t265_node',
+        name='rs_t265',
         output='screen'
-        )
-
-    d400_node = Node(
+    ),
+    Node(
         package='realsense_ros2',
-        node_executable='rs_d435_node',
-        node_name='rs_d435',
+        executable='rs_d435_node',
+        name='rs_d435',
         output='screen',
         parameters=[
             {"publish_depth": True},
@@ -44,43 +43,37 @@ def generate_launch_description():
             {"is_color": True},
             {"publish_image_raw_": True},
             {"fps": 30}      # Can only take values of 6,15,30 or 60
-            ]
-        )
+        ]
+    ),
+    Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 't265_frame', 'base_link']
+    ),
+    Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        # Check this to make sure the transform is correct.
+        arguments=['0.0', '0.025', '0.03', '1.5708', '0.0', '1.5708', 'base_link', 'camera_link_d435']
+    ),
+    Node(
+        package='rtabmap_ros', executable='rtabmap', output='screen',
+        parameters=parameters,
+        remappings=remappings,
+        arguments=['-d']
+    ),
+    Node(
+        package='rtabmap_ros', executable='rtabmapviz', output='screen',
+        parameters=parameters,
+        remappings=remappings
+    ),
 
-    tf265_tf_node = Node(
-            package='tf2_ros',
-            node_executable='static_transform_publisher',
-            output='screen',
-            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 't265_frame', 'base_link']
-        ),
+    SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1'),
 
-    camera_link_tf_node = Node(
-            package='tf2_ros',
-            node_executable='static_transform_publisher',
-            output='screen',
-            # Check this to make sure the transform is correct.
-            arguments=['0.0', '0.025', '0.03', '1.5708', '0.0', '1.5708', 'base_link', 'camera_link_d435']
-        )
-
-    rtabmap_node = Node(
-            package='rtabmap_ros', node_executable='rtabmap', output='screen',
-            parameters=parameters,
-            remappings=remappings,
-            arguments=['-d'])
-
-    # Camera nodes
-    ld.add_action(t265_node, d400_node)
-
-    # Transform nodes
-    ld.add_action(tf265_tf_node, camera_link_tf_node)
-
-    # Localization nodes
-    ld.add_action(rtabmap_node)
-
-    ld.SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1'),
-
-    ld.DeclareLaunchArgument(
+    DeclareLaunchArgument(
             'use_sim_time', default_value='false',
             description='Use simulation (Gazebo) clock if true')
 
-    return ld
+    ])
