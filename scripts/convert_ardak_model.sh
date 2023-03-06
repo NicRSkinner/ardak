d5180413771a168e89a716c14c8b3e7275c244f1
NicRSkinner <nicholas.skinner95@gmail.com>
2023-03-05 20:02:55 -0600

    Added conversion script file. * Begun implementation of GPS simulation.

---
+++ scripts/convert_ardak_model.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -z "$MDIR" ]; then
+    MDIR=../description/ardak
+fi
+
+xacro $MDIR/ardak.urdf > $MDIR/model.urdf
+gz sdf -p $MDIR/model.urdf > $MDIR/model.sdf
+
+sed -i -e "/<model name='ardak'>/r $MDIR/realsense_camera.sdf" $MDIR/model.sdf
