075f4cbcf3351c487e88c3bd4a2538a788bef2cb
NicRSkinner <nicholas.skinner95@gmail.com>
2023-03-13 13:17:18 -0500

    Conversion file from URDF ardak to SDF

--- scripts/convert_ardak_model.sh
+++ scripts/convert_ardak_model.sh
@@ -7,3 +7,4 @@ xacro $MDIR/ardak.urdf > $MDIR/model.urdf
 gz sdf -p $MDIR/model.urdf > $MDIR/model.sdf
 
 sed -i -e "/<model name='ardak'>/r $MDIR/realsense_camera.sdf" $MDIR/model.sdf
+sed -i -e "/<model name='ardak'>/r $MDIR/gps.sdf" $MDIR/model.sdf
