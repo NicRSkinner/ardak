a1dc4f889e1c29199a5dcc2f2a4aa0992a6822b5
NicRSkinner <nicholas.skinner95@gmail.com>
2023-03-14 14:48:31 -0500

    Added IMU sensor with orientation to SDF.

--- scripts/convert_ardak_model.sh
+++ scripts/convert_ardak_model.sh
@@ -8,3 +8,4 @@ gz sdf -p $MDIR/model.urdf > $MDIR/model.sdf
 
 sed -i -e "/<model name='ardak'>/r $MDIR/realsense_camera.sdf" $MDIR/model.sdf
 sed -i -e "/<model name='ardak'>/r $MDIR/gps.sdf" $MDIR/model.sdf
+sed -i -e "/<model name='ardak'>/r $MDIR/imu.sdf" $MDIR/model.sdf
