b041868df019789386bf1f253d1ba03c622abd6f
NicRSkinner <nicholas.skinner95@gmail.com>
2023-03-18 15:24:56 -0500

    Corrected SDF conversion

--- scripts/convert_ardak_model.sh
+++ scripts/convert_ardak_model.sh
@@ -3,9 +3,10 @@ if [ -z "$MDIR" ]; then
     MDIR=../description/ardak
 fi
 
+
 xacro $MDIR/ardak.urdf > $MDIR/model.urdf
 gz sdf -p $MDIR/model.urdf > $MDIR/model.sdf
 
 sed -i -e "/<model name='ardak'>/r $MDIR/realsense_camera.sdf" $MDIR/model.sdf
-sed -i -e "/<model name='ardak'>/r $MDIR/gps.sdf" $MDIR/model.sdf
-sed -i -e "/<model name='ardak'>/r $MDIR/imu.sdf" $MDIR/model.sdf
+#sed -i -e "/<model name='ardak'>/r $MDIR/gps.sdf" $MDIR/model.sdf
+#sed -i -e "/<model name='ardak'>/r $MDIR/imu.sdf" $MDIR/model.sdf
